238e2ba3df579fd54085b8524e537ce272753b11
Guangyuan(Frank) Li <frankli296@yahoo.com>
2021-10-06 00:07:30 -0400

    try mock

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,10 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 
-autodoc_mock_imports = ['seaborn','anytree','scipy','scanpy','anndata','gseapy','scrublet','yattag']
+import mock
+MOCK_MODULES = ['pandas','numpy','matplotlib','seaborn','anytree','scipy','scanpy','anndata','gseapy','scrublet']
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = mock.Mock()
 
 
 # -- Project information -----------------------------------------------------
