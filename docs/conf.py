718ffe6cf93fc22808c892d246f5e7872b4352e6
Guangyuan(Frank) Li <frankli296@yahoo.com>
2022-01-07 14:05:57 -0500

    0.10.1 in doc

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ copyright = '2021, Guangyuan(Frank) Li'
 author = 'Guangyuan(Frank) Li'
 
 # The full version, including alpha/beta/rc tags
-release = '0.10.0'
+release = '0.10.1'
 
 
 # -- General configuration ---------------------------------------------------
