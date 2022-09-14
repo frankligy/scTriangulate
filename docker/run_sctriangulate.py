519500daa0e31d1264528777992234ab52f9c0da
Guangyuan(Frank) Li <frankli296@yahoo.com>
2022-09-14 12:17:21 -0400

    fix docker entrypoint

--- docker/run_sctriangulate.py
+++ docker/run_sctriangulate.py
@@ -23,6 +23,6 @@ if __name__ == '__main__':
     parser.add_argument('--adata_path',type=str,default=None,help='path to the h5ad file')
     parser.add_argument('--dir_path',type=str,default=None,help='path to the result folder, does not need to be present')
     parser.add_argument('--query',nargs='+',default=None,help='specifiy annotations to triangulate, seperated by whitespace')
-    parser.add_argument('--predict_doublet',actions='store_true')
+    parser.add_argument('--predict_doublet',action='store_true')
     args = parser.parse_args()
     main(args)
